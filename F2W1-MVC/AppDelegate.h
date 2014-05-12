a334f68e02d8d8ac5f59cf18704efc8aec36c603
lee-leonardo <leoleez@hotmail.com>
2014-05-12 13:41:50 -0700

    Initial Commit

---
+++ F2W1-MVC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  F2W1-MVC
+//
+//  Created by Leonardo Lee on 5/12/14.
+//  Copyright (c) 2014 Leonardo Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ F2W1-MVC/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  F2W1-MVC
+//
+//  Created by Leonardo Lee on 5/12/14.
+//  Copyright (c) 2014 Leonardo Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
